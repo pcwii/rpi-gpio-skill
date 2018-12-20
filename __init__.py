3a8191b53dd8596d260bd638e4d3aab89cca7612
philc <philc@arcx.com>
2018-12-20 18:11:23 -0500

    update gpio pin numbers

--- __init__.py
+++ __init__.py
@@ -40,7 +40,7 @@ class GPIOSkill(MycroftSkill):
         if str_remainder.find('for') or str_remainder.find('four'):
             str_limits = [4]
         else:
-        str_limits = re.findall('\d+', str_remainder)
+            str_limits = re.findall('\d+', str_remainder)
         if str_limits:
             gpio_request = int(str_limits[0])
             if (gpio_request > 1) and (gpio_request < 28):
